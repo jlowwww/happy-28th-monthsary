7e98fda164b607a40b39e3bce46ff3566fffbcac
jlowwww <johnlowcip@gmail.com>
2024-05-16 11:58:23 +0800

    Add files via upload

---
+++ lala.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
+
+window.addEventListener('click', () =>{
+    document.getElementById("aa").play();
+});
